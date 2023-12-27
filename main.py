5af268ba90f9eb9f5912ba1208e4555dd9ef4f46
OtwakO <78617209+tom28055101@users.noreply.github.com>
2023-12-27 11:32:22 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,14 +1,41 @@
-from typing import Optional
+# -*- coding: utf-8 -*-
+from contextlib import asynccontextmanager
+import os, sys, time
 
+os.chdir(os.path.dirname(os.path.abspath(sys.argv[0])))
+
+import nest_asyncio
 from fastapi import FastAPI
+from deepl_api import DeeplTranslator
 
-app = FastAPI()
+nest_asyncio.apply()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    global deeplEngine, timestamp_check
+    timestamp_check = int(time.time())
+    deeplEngine = DeeplTranslator()
+    print("lifespan startup")
+    yield
+    print("Test")
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+
+app = FastAPI(lifespan=lifespan)
+
+
+@app.post("/deepl")
+async def translate_sentences(data: dict):
+    global timestamp_check, deeplEngine
+    current_timestamp = int(time.time())
+    if current_timestamp - timestamp_check > 60 * 30:
+        deeplEngine = DeeplTranslator()
+        timestamp_check = int(time.time())
+    print(data)
+    source_lang = data.get("source_lang", "JP")
+    target_lang = data.get("target_lang", "EN")
+    response = deeplEngine.translate(
+        data["Raw"], source_lang=source_lang, target_lang=target_lang
+    )
+    print(response)
+    return {"Translation": response}
